d73464aeccd18f42132a16475d0b30ba80ac0764
quanghuy1242 <katygaga124@gmail.com>
2019-03-24 22:58:46 +0700

    redesign action bar post crud page

--- src/material-module.ts
+++ src/material-module.ts
@@ -16,7 +16,8 @@ import {
   MatBadgeModule,
   MatGridListModule,
   MatRippleModule,
-  MatTabsModule
+  MatTabsModule,
+  MatButtonToggleModule
 } from '@angular/material';
 
 @NgModule({
@@ -37,7 +38,8 @@ import {
     MatBadgeModule,
     MatGridListModule,
     MatRippleModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonToggleModule
   ]
 })
 export class MaterialModule {}
